fd58d3cf4571765173bc014cbac3bb6537740725
Satheesh <satheeshvenkat9952@gmail.com>
2023-01-05 09:36:57 +0530

    code updation

--- app/controllers/calci/historydisplay.js
+++ app/controllers/calci/historydisplay.js
@@ -1,5 +1,12 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { action } from '@ember/object'
 
 export default class HistorydisplayController extends Controller {
     welcome = "this is history page";
+    @service history;
+    @action
+    demo(){
+        alert("hello");
+    }
 }
